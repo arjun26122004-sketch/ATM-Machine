2ff4624e79e52a7edd46e87c995569daf5476871
arjun26122004-sketch <arjun26122004@gmail.com>
2025-11-26 15:47:20 +0530

    Add files via upload

---
+++ ATM/src/com/Arjun/Atminterface.java
@@ -0,0 +1,8 @@
+package com.Arjun;
+
+public interface Atminterface {
+	 public void viewBalance();
+	    public void withdrawAmount(double withdrawAmount);
+	    public void depositAmount(double depositAmount );
+	    public  void viewMiniStatement();
+}
